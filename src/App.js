1eb3677180606de103b60019babeffceef3f13fd
SanzinaHossain <sanzina67hossain@gmail.com>
2022-05-14 00:31:20 +0600

    navbar class add

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Navbar from './Pages/Shared/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appoinment';
+import Contact from './Pages/Contact/Contact';
+import Login from './Pages/Login/Login';
+import Review from './Pages/Review/Review';
 function App() {
   return (
     <div>
      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/appoinment" element={<Appointment></Appointment>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/review" element={<Review></Review>}></Route>
         <Route path="/about" element={<About></About>}></Route>
       </Routes>
     </div>
